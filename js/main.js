97c0096d652ae4cf9c4b1a9106bb3595dd14a8ab
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2021-11-13 22:49:11 +0100

    update main.js add cookies suport

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ const sideMenu = document.querySelector("#nav_menu");
 const lightSwitch = document.querySelector("#switch_input");
 const body = document.querySelector("body");
 var isMenuOpen = false;
-var isDarkModeToggle = false;
 
 function toggleMenu() {
   openMenu.addEventListener("click", function() {
@@ -22,15 +21,41 @@ function toggleLight() {
   lightSwitch.addEventListener("click", function() {
     if(lightSwitch.checked == true) {
       body.classList.add("dark");
-      isDarkModeToggle = true;
+      createCookie("isDarkModeToggle", "true")
       if(isMenuOpen) {sideMenu.classList.remove("active");}
     } else {
       body.classList.remove("dark");
-      isDarkModeToggle = false;
+      createCookie("isDarkModeToggle", "false")
       if(isMenuOpen) {sideMenu.classList.remove("active");}
     }
   })
 }
 
-toggleMenu()
+function createCookie(name, value, days) {
+  if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      var expire = "; expire=" + date.toGMTString();
+  } else {var expire = ""}
+
+  document.cookie = name + "=" + value + expire + "; path=/";
+}
+
+function getCookieValue(name) {
+  var re = new RegExp(name + "=([^;]+)");
+  var value = re.exec(document.cookie);
+  return (value != null) ? unescape(value[1]) : null;
+}
+
+function isDarkModeToggle() {
+  if (getCookieValue("isDarkModeToggle") == "false"){
+    body.classList.remove("dark");
+  } else if (getCookieValue("isDarkModeToggle") == "true") {
+    body.classList.add("dark");
+    lightSwitch.checked = true;
+  }
+}
+
+isDarkModeToggle()
 toggleLight()
+toggleMenu()
