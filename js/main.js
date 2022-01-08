058b34ba2368f60f98b77c1f15e403ef3cf62e7b
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2022-01-08 13:05:03 +0100

    🔥add the resume section

--- js/main.js
+++ js/main.js
@@ -2,6 +2,16 @@ const openMenu = document.querySelector("#show_menu");
 const hideMenu = document.querySelector("#hide_menu");
 const sideMenu = document.querySelector("#nav_menu");
 
+const resumeContainer = document.querySelector("#resume_container");
+const resumeSection = document.querySelector("#resume_section");
+const resumeFr = document.querySelector("#resume_fr");
+const resumeGb = document.querySelector("#resume_gb");
+
+const resumeFrButton = document.querySelector("#resume_fr_button");
+const resumeGbButton = document.querySelector("#resume_gb_button");
+const resumeFrReturn = document.querySelector("#resume_fr_return");
+const resumeGbReturn = document.querySelector("#resume_gb_return");
+
 function toggleMenu() {
   openMenu.addEventListener("click", function() {
     sideMenu.classList.add("active");
@@ -10,6 +20,10 @@ function toggleMenu() {
   document.addEventListener("keydown", function(event) {
     if (event.key == " " || event.key == "ArrowRight")
     {
+      sideMenu.classList.add("active");
+    }
+
+    if (document.activeElement == openMenu && event.key == "Enter") {
       sideMenu.classList.add("active");
     }
   })
@@ -26,4 +40,31 @@ function toggleMenu() {
   })
 }
 
+function toggleResume() {
+  resumeFrButton.addEventListener("click", function() {
+    resumeSection.classList.add("hide");
+    resumeContainer.classList.add("change");
+    resumeFr.classList.add("display");
+  })
+
+  resumeFrReturn.addEventListener("click", function() {
+    resumeSection.classList.remove("hide");
+    resumeContainer.classList.remove("change");
+    resumeFr.classList.remove("display");
+  })
+
+  resumeGbButton.addEventListener("click", function() {
+    resumeSection.classList.add("hide");
+    resumeContainer.classList.add("change");
+    resumeGb.classList.add("display");
+  })
+
+  resumeGbReturn.addEventListener("click", function() {
+    resumeSection.classList.remove("hide");
+    resumeContainer.classList.remove("change");
+    resumeGb.classList.remove("display");
+  })
+}
+
 toggleMenu()
+toggleResume()
