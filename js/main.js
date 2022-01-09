017dee7c2a9a8edf25483a532ce91dc17e16d5cb
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2022-01-10 00:07:16 +0100

    ⚙️add some more input control

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ const openMenu = document.querySelector("#show_menu");
 const hideMenu = document.querySelector("#hide_menu");
 const sideMenu = document.querySelector("#nav_menu");
 
+const menuButtons = sideMenu.querySelectorAll("a");
+
 const resumeContainer = document.querySelector("#resume_container");
 const resumeSection = document.querySelector("#resume_section");
 const resumeFr = document.querySelector("#resume_fr");
@@ -15,27 +17,68 @@ const resumeGbReturn = document.querySelector("#resume_gb_return");
 function toggleMenu() {
   openMenu.addEventListener("click", function() {
     sideMenu.classList.add("active");
+    openMenu.setAttribute("tabindex", "-1");
+    hideMenu.setAttribute("tabindex", "1");
+    
+    for (var i = 0; i < menuButtons.length; i++) {
+      menuButtons[i].setAttribute("tabindex", "1");
+    }
   })
 
   document.addEventListener("keydown", function(event) {
     if (event.key == " " || event.key == "ArrowRight")
     {
       sideMenu.classList.add("active");
+      openMenu.setAttribute("tabindex", "-1");
+      hideMenu.setAttribute("tabindex", "1");
+      
+      for (var i = 0; i < menuButtons.length; i++) {
+        menuButtons[i].setAttribute("tabindex", "1");
+      }
     }
 
     if (document.activeElement == openMenu && event.key == "Enter") {
       sideMenu.classList.add("active");
+      openMenu.setAttribute("tabindex", "-1");
+      hideMenu.setAttribute("tabindex", "1");
+      
+      for (var i = 0; i < menuButtons.length; i++) {
+        menuButtons[i].setAttribute("tabindex", "1");
+      }
+      menuButtons[0].focus();
+    }
+
+    if (document.activeElement == hideMenu && event.key == "Enter") {
+      sideMenu.classList.remove("active");
+      openMenu.setAttribute("tabindex", "1");
+      hideMenu.setAttribute("tabindex", "-1");
+      
+      for (var i = 0; i < menuButtons.length; i++) {
+        menuButtons[i].setAttribute("tabindex", "-1");
+      }
     }
   })
 
   hideMenu.addEventListener("click", function() {
     sideMenu.classList.remove("active");
+    openMenu.setAttribute("tabindex", "1");
+    hideMenu.setAttribute("tabindex", "-1");
+
+    for (var i = 0; i < menuButtons.length; i++) {
+      menuButtons[i].setAttribute("tabindex", "-1");
+    }
   })
 
   document.addEventListener("keydown", function(event) {
     if (event.key == "Escape" || event.key == "ArrowLeft")
     {
       sideMenu.classList.remove("active");
+      openMenu.setAttribute("tabindex", "1");
+      hideMenu.setAttribute("tabindex", "-1");
+
+      for (var i = 0; i < menuButtons.length; i++) {
+        menuButtons[i].setAttribute("tabindex", "-1");
+      }
     }
   })
 }
