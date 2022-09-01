9a1e42da3bc374a7cb52e230be9dd85b3850e9d9
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2022-09-01 14:52:14 +0200

    ADDED: holidays events are celebrated

--- js/index.js
+++ js/index.js
@@ -5,15 +5,37 @@ const today = new Date();
 function toggleAutoAge() {
     var age = today.getFullYear() - birthday.getFullYear();
 
-    if (today.getMonth() < birthday.getMonth() || (today.getMonth() == birthday.getMonth() && today.getDate() < birthday.getDate())) {
+    if (today.getMonth()+1 < birthday.getMonth() || (today.getMonth()+1 == birthday.getMonth() && today.getDate() < birthday.getDate())) {
         age--;
     }
 
-    if (today.getDate() == birthday.getDate() && today.getMonth() == birthday.getMonth()) {
+    if (today.getDate() == birthday.getDate() && today.getMonth()+1 == birthday.getMonth()) {
         ageText.innerText = `🎂${age} years old🎂`;
     } else {
         ageText.innerText = `${age} years old`;
     }
 }
 
+const easterEvent = document.querySelector("#easter");
+const easter = new Date(0, 4, 9);
+const halloweenEvent = document.querySelector("#halloween");
+const halloween = new Date(0, 10, 31);
+const christmasEvent = document.querySelector("#christmas");
+const christmas = new Date(0, 12, 25);
+
+function toggleHollidaysEvents() {
+    if (today.getDate() == easter.getDate() && today.getMonth()+1 == easter.getMonth()) {
+        easterEvent.classList.add("active");
+    }
+    
+    if (today.getDate() == halloween.getDate() && today.getMonth()+1 == halloween.getMonth()) {
+        halloweenEvent.classList.add("active");
+    }
+
+    if (today.getDate() == christmas.getDate() && today.getMonth()+1 == christmas.getMonth()) {
+        christmasEvent.classList.add("active");
+    }
+}
+
 toggleAutoAge();
+toggleHollidaysEvents();
